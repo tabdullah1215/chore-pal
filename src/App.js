861e3c20f81d98c37f3a8f5b507f838ce8a946b8
Tariq Abdullah <tabdullah1215@gmail.com>
2024-09-26 18:59:55 -0400

    moved amplifyconfiguration.json to public folder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import config from './amplifyconfiguration.json';
 import Dashboard from './components/Dashboard';
 import InviteAccept from './components/InviteAccept';
 
-Amplify.configure(config);
-
 function App() {
+    const [isConfigured, setIsConfigured] = useState(false);
+
+    useEffect(() => {
+        fetch('/amplifyconfiguration.json')
+            .then(response => response.json())
+            .then(config => {
+                Amplify.configure(config);
+                setIsConfigured(true);
+            })
+            .catch(error => console.error('Error loading Amplify configuration:', error));
+    }, []);
+
+    if (!isConfigured) {
+        return <div>Loading configuration...</div>;
+    }
+
     return (
         <Authenticator>
             {({ signOut, user }) => (
